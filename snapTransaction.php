7faf6870f33bdf524c89820cd29d8892ec88fdfb
Bayu Muhammad Iqbal <42398906+bayuiqballl@users.noreply.github.com>
2021-08-01 18:05:32 +0700

    first commit

---
+++ snapTransaction.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+// Set your Merchant Server Key
+
+\Midtrans\Config::$serverKey = 'SB-Mid-server-0okIB1VvdGzlhGhAeUO28Y5Y';
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+
+
+ 
+$params = array(
+    'transaction_details' => array(
+        'order_id' => rand(),
+        'gross_amount' => 10000,
+    ),
+    'customer_details' => array(
+        'first_name' => 'Raayu',
+        'last_name' => 'Muhammad Iqbal',
+        'email' => 'pra@example.com',
+        'phone' => '08111228888',
+    ),
+);
+ 
+// $snapToken = \Midtrans\Snap::getSnapToken($params);
+$snapUrlRedirect = \Midtrans\Snap::createTransaction($params)->redirect_url;
+
+
+echo $link;
